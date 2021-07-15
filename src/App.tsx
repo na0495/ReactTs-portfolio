36c06f20943ae9bd1f18c984f738b1d88f294ef9
na0495 <saadmrabet008@gmail.com>
2021-07-15 21:30:35 +0100

    adding dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import ThemeProvider from './theme/ThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import Home from './components/Home';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      type: 'light'
+    }
+  });
+
   return (
-    <div>
-      <p>Hello world !</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import { useChangeTheme } from '../theme/ThemeProvider';
+import useTheme from '@material-ui/core/styles/useTheme';
+
+const Home: React.FC = ()=>{
+  const theme = useTheme();
+  const changeTheme = useChangeTheme();
+  return (
+    <div style={{height: 720}}>
+        <IconButton
+        title="Toggle light/dark mode"
+        onClick={()=>changeTheme()}
+        >
+        {theme.palette.type === 'light' ? <Brightness4Icon /> : <Brightness7Icon />}
+        </IconButton>
+    </div>
+  )
+}
+
+export default Home;
