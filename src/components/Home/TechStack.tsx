cd3da56ee074a746966417f336f0a7d5d0192ab2
na0495 <saadmrabet008@gmail.com>
2021-07-26 16:27:36 +0100

    update tech satck

--- src/components/Home/TechStack.tsx
+++ src/components/Home/TechStack.tsx
@@ -1,55 +1,100 @@
-import { Box, Card, Grid, Typography } from "@material-ui/core";
+import { Card, CardContent, CardMedia, Chip, Container, Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Skill from "../layout/Skill";
+// import Skill from "../layout/Skill";
 
 const useStyles = makeStyles(theme => ({
-    heading: {
-        fontWeight: "bold",
-        display: "flex",
-        justifyContent: "center",
-        alignContent: "center",
-        padding: theme.spacing(5),
-    },
-    center: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    card: {
-      borderRadius: 25
-    },
-    skill: {
-      padding: 3,
-      width: 'auto'
-    }
+  container: {
+    marginTop: theme.spacing(5),
+  },
+  heading: {
+      fontWeight: "bold",
+      display: "flex",
+      justifyContent: "center",
+      alignContent: "center",
+      padding: theme.spacing(5),
+  },
+  card: {
+    marginTop: theme.spacing(5),
+    borderRadius: 25,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: theme.spacing(5),
+  },
+  skill: {
+    padding: 3,
+    width: 'auto'
+  },
+  image: {
+    width: 175,
+    height: 175,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  chip: {
+    margin: 2,
+    border: '2px solid'
+  }
 }));
 
 
 export default function TechStack() {
     const classes = useStyles();
+    const Techno = [
+      {
+        id: 1,
+        name: 'Front-end',
+        tech: ['ReactJs', 'Redux', 'Angular', 'Bootstrap', 'Material-UI', 'Chakra-UI'],
+        img: '/stack/frontend.png'
+      },
+      {
+        id: 2,
+        name: 'Back-end',
+        tech: ['Django', 'Laravel'],
+        img: '/stack/backend.png'
+      },
+      {
+        id: 3,
+        name: 'Data base',
+        tech: ['MySQL', 'PostgreSQL', 'SQLite'],
+        img: '/stack/database.jpg'
+      },
+    ]
 
     return (
-        <>
-        <Card className={classes.card}>
+        <Container className={classes.container}>
         <Typography variant="h4" className={classes.heading}>
           Tech Stack :
         </Typography>
-        <Box className={classes.center}>
-        <Grid container>
-          <Grid item xs={12} sm={6}>
-            <Card className={classes.skill}>
-              <Skill
-                name='Front-end'
-                detail='JavaScript, React, Typescript, Redux, Redux thunk, Angular'
-                icon='/language/react.png'
-              />
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-          </Grid>
-        </Grid>
-        </Box>
-      </Card>
+        <div className={classes.details}>
+        {Techno.map((p, i) => (
+          <Card key={i} className={classes.card}>
+            <CardMedia
+              className={classes.image}
+              component="img"
+              alt="Back-end"
+              image={process.env.PUBLIC_URL + p.img}
+              title="Back-end"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {p.name}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+              {p.tech.map((p, i) => {
+                return (
+                  <Chip key={i} className={classes.chip} label={p}/>
+                );
+              })}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+        </div>
             <Grid container>
               {/* <Grid item xs={12}>
                 <motion.img style={{ width: 45, height: 45, borderRadius: 15}} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} src="/language/python.png" />
@@ -72,6 +117,6 @@ export default function TechStack() {
                 <motion.img style={{ width: 45, height: 45, borderRadius: 15}} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} src="/language/laravel.png" />
               </Grid> */}
             </Grid>
-        </>
+        </Container>
     )
 }
